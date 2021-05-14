eb0956cea2b18822382002baaefe5d94798ca758
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-05-14 09:55:00 +0100

    Add error if API doesn't work.

--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,31 @@
-import {Types} from "ably/ably";
-import * as Ably from "ably/promises";
+import { Types } from 'ably/ably';
+import * as Ably from 'ably/promises';
 
 // Update the API key
-const ABLY_API_KEY = undefined
+const ABLY_API_KEY = undefined;
 
 export default class Auth {
-    private ably: Ably.Rest;
+  private ably: Ably.Rest;
 
-    constructor() {
-        if (!ABLY_API_KEY) {
-            console.warn(`api key is ${ABLY_API_KEY}`)
-        }
-        const clientOptions: Ably.Types.ClientOptions = {
-            key: ABLY_API_KEY
-        }
-        this.ably = new Ably.Rest(clientOptions);
+  constructor() {
+    if (!ABLY_API_KEY) {
+      throw new Error(
+        `You need update ABLY_API_KEY, its currently ${ABLY_API_KEY}`
+      );
     }
+    const clientOptions: Ably.Types.ClientOptions = {
+      key: ABLY_API_KEY,
+    };
+    this.ably = new Ably.Rest(clientOptions);
+  }
 
-    createToken = async (clientId: string): Promise<Types.TokenDetails | void> => {
-        try {
-            return await this.ably.auth.requestToken({clientId})
-        } catch (e) {
-            console.error(e)
-        }
+  createToken = async (
+    clientId: string
+  ): Promise<Types.TokenDetails | void> => {
+    try {
+      return await this.ably.auth.requestToken({ clientId });
+    } catch (e) {
+      console.error(e);
     }
+  };
 }
